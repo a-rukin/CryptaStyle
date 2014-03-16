153dbecfbced878bb2aba011ce525e9f772eb9a1
Ivan Samborskiy <whiplash1993@yandex.ru>
2014-03-16 16:53:28 +0400

    added getters

---
+++ GitHub/CryptaStyleApp/src/main/java/com/airwhip/cryptastyle/getters/ApplicationInformation.java
@@ -0,0 +1,32 @@
+package com.airwhip.cryptastyle.getters;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+
+import java.util.List;
+
+/**
+ * Created by Whiplash on 05.03.14.
+ */
+public class ApplicationInformation {
+
+    private static final String MAIN_TAG_BEGIN = "<application>\n";
+    private static final String MAIN_TAG_END = "</application>\n";
+
+    private static final String NAME_TAG_BEGIN = "\t<name>";
+    private static final String NAME_TAG_END = "</name>\n";
+
+    public static StringBuilder get(Context context) {
+        StringBuilder result = new StringBuilder(MAIN_TAG_BEGIN);
+
+        final PackageManager pm = context.getPackageManager();
+        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+        for (ApplicationInfo packageInfo : packages) {
+            result.append(NAME_TAG_BEGIN + packageInfo.packageName + NAME_TAG_END);
+        }
+
+        return result.append(MAIN_TAG_END);
+    }
+
+}
---
+++ GitHub/CryptaStyleApp/src/main/java/com/airwhip/cryptastyle/getters/MusicInformation.java
@@ -0,0 +1,62 @@
+package com.airwhip.cryptastyle.getters;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.provider.MediaStore;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Whiplash on 05.03.14.
+ */
+public class MusicInformation {
+
+    private static final String IS_MUSIC = MediaStore.Audio.Media.IS_MUSIC + " != 0";
+    private static final String[] PROJECTION = {MediaStore.Audio.Media.ARTIST};
+
+    private static final String MAIN_TAG_BEGIN = "<music>\n";
+    private static final String MAIN_TAG_END = "</music>\n";
+
+    private static final String ITEM_TAG_BEGIN = "\t<item>\n";
+    private static final String ITEM_TAG_END = "\t</item>\n";
+
+    private static final String NAME_TAG_BEGIN = "\t\t<name>";
+    private static final String NAME_TAG_END = "</name>\n";
+
+    private static final String COUNT_TAG_BEGIN = "\t\t<count>";
+    private static final String COUNT_TAG_END = "</count>\n";
+
+    public static StringBuilder get(Context context) {
+        StringBuilder result = new StringBuilder(MAIN_TAG_BEGIN);
+
+        List<String> artistsList = new ArrayList<>();
+        Map<String, Integer> artistsStorage = new HashMap<>();
+
+        Cursor cursor = context.getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, PROJECTION, IS_MUSIC, null, null);
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                String artist = cursor.getString(0);
+                if (!artistsStorage.containsKey(artist)) {
+                    artistsList.add(artist);
+                    artistsStorage.put(artist, 1);
+                } else {
+                    int count = artistsStorage.remove(artist);
+                    artistsStorage.put(artist, count + 1);
+                }
+            }
+        }
+
+        for (String artist : artistsList) {
+            result.append(ITEM_TAG_BEGIN);
+            result.append(NAME_TAG_BEGIN + artist + NAME_TAG_END);
+            result.append(COUNT_TAG_BEGIN + artistsStorage.get(artist) + COUNT_TAG_END);
+            result.append(ITEM_TAG_END);
+        }
+
+        return result.append(MAIN_TAG_END);
+    }
+
+}
