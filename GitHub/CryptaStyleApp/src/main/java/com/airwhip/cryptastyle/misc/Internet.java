6eef2cc0e707bce5cf4b5a3a8f7c828c9ddb64c7
Ivan Samborskiy <whiplash1993@yandex.ru>
2014-03-17 19:15:11 +0400

    added check internet connection

---
+++ GitHub/CryptaStyleApp/src/main/java/com/airwhip/cryptastyle/misc/Internet.java
@@ -0,0 +1,25 @@
+package com.airwhip.cryptastyle.misc;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by Whiplash on 17.03.14.
+ */
+public class Internet {
+    private static Internet ourInstance = new Internet();
+
+    public static Internet getInstance() {
+        return ourInstance;
+    }
+
+    private Internet() {
+    }
+
+    public static boolean checkInternetConnection(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+}
